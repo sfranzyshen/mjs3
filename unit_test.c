6133d31a99276e3fb4a0048f87c8a9a84bfe1954
Sergey Lyubka <lsm@cesanta.com>
2018-10-27 16:48:53 +0100

    Add strings unit test

--- unit_test.c
+++ unit_test.c
@@ -1,9 +1,17 @@
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 #include "mjs.h"
 
 static int check_num(mjs_val_t v, float expected) {
   return mjs_is_number(v) && fabs(mjs_get_number(v) - expected) < 0.0001;
+}
+
+static int check_str(struct mjs *mjs, mjs_val_t v, const char *expected) {
+  int n;
+  return mjs_is_string(v) &&
+         strcmp(mjs_get_string(mjs, v, &n), expected) == 0 &&
+         n == (int) strlen(expected);
 }
 
 static void test_expr(void) {
@@ -17,9 +25,19 @@ static void test_expr(void) {
   assert(check_num(v, 1.23));
   assert(mjs_exec(mjs, "a - 2 * 3.1;", &v) == MJS_SUCCESS);
   assert(check_num(v, -4.97));
+  mjs_destroy(mjs);
+}
+
+static void test_strings(void) {
+  struct mjs *mjs = mjs_create();
+  mjs_val_t v;
+  assert(mjs_exec(mjs, "'a' + 'b'", &v) == MJS_SUCCESS);
+  assert(check_str(mjs, v, "ab"));
+  mjs_destroy(mjs);
 }
 
 int main(void) {
   test_expr();
+  test_strings();
   return 0;
 }
