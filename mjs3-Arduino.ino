9229ae714d4a617fbbca7df195d25c4562401a74
Scotty Franzyshen <sfranzyshen@gmail.com>
2019-08-19 16:01:23 -0700

    Update mjs3-Arduino.ino

--- mjs3-Arduino.ino
+++ mjs3-Arduino.ino
@@ -1,5 +1,5 @@
 #define MJS_STRING_POOL_SIZE 200      // Buffer for all strings
-#include "mjs3.c"                     
+#include "mjs3-Arduino.c"                     
 
 extern void myDelay(int x) { 
   delay(x);
